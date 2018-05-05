26dab798d70a0d97af9a6aa63d532caf6c53c12f
Kiran Lorenzo <kiran.lorenzo@live.u-tad.com>
2018-05-05 21:35:47 +0200

    First Commit

---
+++ src/prueba1/parte1.java
@@ -0,0 +1,20 @@
+package prueba1;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class parte1 {
+    private JTextArea proyecto1TextArea;
+    private JButton button1;
+
+    public parte1() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+}
